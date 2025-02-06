30a857ce0359dfb2c0994447dcda0599d3f5124d
oxalica <oxalicc@pm.me>
2025-02-06 08:17:59 -0500

    Add i686 and arm in devenv

--- flake.nix
+++ flake.nix
@@ -17,20 +17,37 @@
           };
         in
         pkgs.mkShell {
-          buildInputs = [
-            pkgs.openssl
-          ];
           nativeBuildInputs = [
-            pkgs.pkg-config
-            (pkgs.rust-bin.nightly."2025-02-01".minimal.override {
+            (pkgs.rust-bin.nightly."2025-02-01".default.override {
               targets = [
                 "riscv64gc-unknown-linux-gnu"
                 "aarch64-apple-darwin"
-                "aarch64-unknown-linux-gnu"
+                "i686-unknown-linux-gnu"
+                "arm-unknown-linux-gnueabi"
               ];
-              extensions = [
-                "rust-src"
-                "clippy"
+            })
+          ];
+        };
+
+      devShells.x86_64-linux.i686 =
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            # crossSystem = "i686-linux";
+            overlays = [
+              rust-overlay.overlays.default
+            ];
+          };
+        in
+        pkgs.pkgsi686Linux.mkShell {
+          buildInputs = [
+            pkgs.pkgsi686Linux.gccForLibs.lib
+          ];
+
+          nativeBuildInputs = [
+            (pkgs.rust-bin.nightly."2025-02-01".minimal.override {
+              targets = [
+                "i686-unknown-linux-gnu"
               ];
             })
           ];
